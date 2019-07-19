be1fa2f1983f4067bbe5c35cf63ecd82fa2b58f2
ffWisteria <e175415m@st.u-gakugei.ac.jp>
2019-07-19 13:45:28 +0900

    4_value_passingのまーじ

--- src/main/java/controller/user/NewServlet.java
+++ src/main/java/controller/user/NewServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Timestamp;
 
 @WebServlet("/users/new")
 public class NewServlet extends HttpServlet {
@@ -23,15 +22,17 @@ public class NewServlet extends HttpServlet {
         String password = request.getParameter("password");
 
         //userインスタンスの作成
-        Timestamp currentTime=new Timestamp(System.currentTimeMillis());
-        User user=new User(
+        User user = new User(
                 null,
-                currentTime,
-                currentTime,
+                null,
+                null,
                 name,
                 email,
                 password
         );
+
+        //userをDBに追加
+        user.createUser();
 
         //リクエストスコープにインスタンスを保存
         //"user"という名前でuserインスタンスを保存
---
+++ src/main/java/model/user/Repository.java
@@ -0,0 +1,135 @@
+package model.user;
+
+import lib.mysql.Client;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+// Repository:
+//   Data Access Object（DAO） という感じでとりあえずはよさそう。
+//   Database 層（永続化層）との連携を行うオブジェクトと捉えれば良い。
+public class Repository extends Client {
+    public static void insertUser(User user) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            // SQL 文の用意
+            String sql = "insert into users " +
+                    "(name, email, password, created_at, updated_at)" +
+                    " values (?, ?, ?, ?, ?)";
+
+            // DB へのコネクションを作成する
+            connection = create();
+
+            // 現在時間の取得
+            // user インスタンスの生成
+            Timestamp currentTime =
+                    new Timestamp(System.currentTimeMillis());
+
+            // セキュリティの一つ。
+            // sql文を入力されてしまった時にデータが消えてしまうかもしれないから
+            // 実行するSQL文とパラメータを指定する
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, user.name);//?1
+            preparedStatement.setString(2, user.email);//?2
+            preparedStatement.setString(3, user.password);//?3
+            preparedStatement.setTimestamp(4, currentTime);//?4
+            preparedStatement.setTimestamp(5, currentTime);//?5
+
+            // SQL 文の実行
+            preparedStatement.executeUpdate();
+            return;
+
+        } catch (SQLException e) {
+            // 例外発生時の処理
+            //ログを残す
+            e.printStackTrace();
+            return;
+
+        } finally {
+            // クローズ処理
+            //ファイルを閉じる
+            close(connection, preparedStatement, resultSet);
+        }
+    }
+
+    public static ArrayList<User> selectUsers() {
+        //Userというクラスの型をリスト化している
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            String sql = "select * from users";
+
+            connection = create();
+
+            preparedStatement = connection.prepareStatement(sql);
+
+            // SQL 文を実行し、結果を保存する
+            resultSet = preparedStatement.executeQuery();
+
+            // 結果を User Entity に整形したうえで、users リストに逐一追加する
+            ArrayList<User> users = new ArrayList<>();
+            while (resultSet.next()) {
+                User user = new User(
+                        String.valueOf(resultSet.getString("id")),
+                        resultSet.getTimestamp("created_at"),
+                        resultSet.getTimestamp("updated_at"),
+                        resultSet.getString("name"),
+                        resultSet.getString("email"),
+                        resultSet.getString("password")
+                );
+                //userというリストにデータを追加する
+                users.add(user);
+            }
+            return users;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+
+        } finally {
+            close(connection, preparedStatement, resultSet);
+        }
+    }
+
+    public static User selectUserByID(String id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            String sql = "select * from users where id = ?";
+
+            connection = create();
+
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, Integer.parseInt(id));
+
+            resultSet = preparedStatement.executeQuery();
+
+            User user = null;
+            while (resultSet.next()) {
+                user = new User(
+                        String.valueOf(resultSet.getString("id")),
+                        resultSet.getTimestamp("created_at"),
+                        resultSet.getTimestamp("updated_at"),
+                        resultSet.getString("name"),
+                        resultSet.getString("email"),
+                        resultSet.getString("password")
+                );
+            }
+            return user;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+
+        } finally {
+            close(connection, preparedStatement, resultSet);
+        }
+    }
+}
